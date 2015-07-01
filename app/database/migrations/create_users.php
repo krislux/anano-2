f3bb8240a39661b113c368b3fea6ff5ba5cab4a9
Kris <kl@republica.dk>
2015-07-01 10:36:14 +0200

    Proper new format for test migration

--- app/database/migrations/create_users.php
+++ app/database/migrations/create_users.php
@@ -1,25 +1,28 @@
 <?php
 
-use Anano\Database\Migrations\Migration;
+use Anano\Database\Migrations\MigrationInterface;
 use Anano\Database\Migrations\Table;
 use Anano\Database\Migrations\Serializers\MySql;
 use Anano\Database\Database;
 
-class CreateUsers extends Migration
+class CreateUsers implements MigrationInterface
 {
     public $table = 'users';
     public $disabled = false;
     
     public function up()
     {
-        $table = new Table($this->table, new MySql, new Database);
-        $table->index('id');
-        $table->save();
+        Table::create($this->table, new MySql, new Database, function($table)
+        {
+            $table->primary('id');
+            $table->string('username', 64);
+            $table->string('password', 64);
+            $table->timestamps();
+        });
     }
     
     public function down()
     {
-        $table = new Table($this->table, new MySql, new Database);
-        $table->drop();
+        Table::drop($this->table, new MySql, new Database);
     }
 }
