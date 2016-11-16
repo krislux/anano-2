90ae8f42f72d89c0eb3e2831dc7e76fb756294c1
Kris <kl@republica.dk>
2016-11-16 11:13:09 +0100

    Added: prefer() method to Input, slight update

--- src/Anano/Http/Input.php
+++ src/Anano/Http/Input.php
@@ -4,7 +4,11 @@ namespace Anano\Http;
 
 abstract class Input
 {
-    public static function get($field, $default='')
+    /**
+     * Get one or more values from the current request by keys.
+     * @return string for single key, array for array of keys, or mixed if default is used.
+     */
+    public static function get($field, $default = '')
     {
         if (is_array($field))
         {
@@ -26,11 +30,42 @@ abstract class Input
         return $default;
     }
 
+    /**
+     * Get a single value from the current request, but with a prioritized list of possible keys/aliases.
+     * Useful for allowing several key names or shorthands, e.g. $search = Input::prefer('search', 's');
+     * If you want to include a default value, fields must be passed as an array. Otherwise as argument list.
+     * @return string
+     */
+    public static function prefer($fieldlist, $default = '')
+    {
+        if ( ! is_array($fieldlist))
+        {
+            $fieldlist = func_get_args();
+            $default = null;
+        }
+
+        foreach ($fieldlist as $f)
+        {
+            if (isset($_REQUEST[$f]))
+                return $_REQUEST[$f];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Get if a key exists in the current request.
+     * @return bool
+     */
     public static function has($field)
     {
         return isset($_REQUEST[$field]);
     }
 
+    /**
+     * Get the HTTP verb (GET, POST, etc) of the current request.
+     * @return string
+     */
     public static function method()
     {
         return $_SERVER['REQUEST_METHOD'];
